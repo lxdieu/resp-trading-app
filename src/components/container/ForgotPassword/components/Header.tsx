296fe7ecafa7d489bdd33f33588d8432dcba7cd9
dieulx <dieu.lx@hsc.com.vn>
2023-12-25 17:26:59 +0700

    update

---
+++ src/components/container/ForgotPassword/components/Header.tsx
@@ -0,0 +1,20 @@
+import { styled } from "@mui/system";
+import { ArrowBack } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+const Wrapper = styled("div")({
+  padding: 16,
+  background: "transparent",
+});
+interface IProps {
+  title: string;
+  handleBack: () => void;
+}
+const Header = ({ title, handleBack }: IProps) => {
+  return (
+    <Wrapper>
+      <ArrowBack onClick />
+      <Typography>{title}</Typography>
+    </Wrapper>
+  );
+};
+export default Header;
