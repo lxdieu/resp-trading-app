5a67df229b703a1b2f5b1ebe5a31b7aef8c29b66
xuan dieu <lxdieu90@gmail.com>
2024-01-22 07:02:20 +0700

    update

--- src/themes/light/components/Backdrop.ts
+++ src/themes/light/components/Backdrop.ts
@@ -1,11 +1,11 @@
 const Backdrop = {
   styleOverrides: {
     root: {
-      // backgroundColor: 'rgba(0,0,0,0.5)',
+      zIndex: 9999,
     },
-    invisible:{
-      backgroundColor:'transparent'
-    }
+    invisible: {
+      backgroundColor: "transparent",
+    },
   },
 };
 export default Backdrop;
