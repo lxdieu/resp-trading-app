c3e0dafab33d50689fc0656696d24e1695e85554
xuan dieu <lxdieu90@gmail.com>
2024-02-09 06:58:30 +0700

    update fonts

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -5,12 +5,10 @@ export const sfpro = localFont({
     {
       path: "./sf-pro-display-light.woff2",
       weight: "300",
-      style: "normal",
     },
     {
       path: "./sf-pro-display-regular.woff2",
       weight: "400",
-      style: "normal",
     },
     {
       path: "./sf-pro-display-medium.woff2",
@@ -20,17 +18,16 @@ export const sfpro = localFont({
     {
       path: "./sf-pro-display-semibold.woff2",
       weight: "500",
-      style: "normal",
     },
     {
       path: "./sf-pro-display-bold.woff2",
       weight: "600",
-      style: "normal",
     },
     {
       path: "./sf-pro-display-black.otf",
       weight: "900",
-      style: "normal",
     },
   ],
+  style: "normal",
+  fallback: ["system-ui", "arial"],
 });
