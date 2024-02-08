83e02f729f1dbd0198d5dbf390266dd6c23a920d
xuan dieu <lxdieu90@gmail.com>
2024-02-09 06:51:19 +0700

    update

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -3,32 +3,32 @@ import localFont from "next/font/local";
 export const sfpro = localFont({
   src: [
     {
-      path: "@/public/fonts/sf-pro-display-light.woff2",
+      path: "./sf-pro-display-light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "@/public/fonts/sf-pro-display-regular.woff2",
+      path: "./sf-pro-display-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "@/public/fonts/sf-pro-display-medium.woff2",
+      path: "./sf-pro-display-medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "@/public/fonts/sf-pro-display-semibold.woff2",
+      path: "./sf-pro-display-semibold.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "@/public/fonts/sf-pro-display-bold.woff2",
+      path: "./sf-pro-display-bold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "@/public/fonts/sf-pro-display-black.otf",
+      path: "./sf-pro-display-black.otf",
       weight: "900",
       style: "normal",
     },
